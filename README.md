c21ad2e03a451544d808540df224f9295f941698
Erik Brendel <erik.brendel@gmx.de>
2019-11-08 23:16:03 +0100

    also other IDEs are working

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This project collects all the nice JetBrains splash images, so that you can gaze
 ### Building
 
 To keep clear of copyright challenges, this repo does not store any JetBrains data. Instead, everything is downloaded automatically via `scripts/load.py`. After this script finishes, the generated `index.html` contains the splash screen overview.
+
+_Warning_: This script downloads several gigabytes of data to your disk. Make sure there is enough space, and prepare for a long waiting time.
--- scripts/load.py
+++ scripts/load.py
@@ -14,7 +14,23 @@ from packaging import version
 requests_cache.install_cache()
 
 
-INDEX = "https://data.services.jetbrains.com/products?code=IIU&release.type=release"
+#  the splash images for IDE [0] are found at:
+#  "*.tar.gz!/<root>/lib/[1].jar!/[2].png"
+#  "*.tar.gz!/<root>/lib/[1].jar!/[2]@2x.png"
+#  <VERSION> will be replaced by eg 20192
+IDE = [
+    ('IIU', 'resources', 'idea_logo'),
+    ('WS', 'webstorm', 'artwork/webide_logo'),
+    ('PS', 'phpstorm', 'artwork/webide_logo'),
+    ('PCP', 'pycharm', 'pycharm_logo'),
+    ('CL', 'clion', 'artwork/clion_splash'),
+    ('RD', 'rider', 'rider/artwork/Rider_<VERSION>_splash'),
+    ('RM', 'rubymine', 'artwork/rubymine_logo'),
+]
+
+IDE_NAMES = [ide[0] for ide in IDE]
+IDE_NAMES_STR = ",".join(IDE_NAMES)
+INDEX = "https://data.services.jetbrains.com/products?code=" + IDE_NAMES_STR + "&release.type=release"
 
 
 def download_with_progress(url: str, file_name: str):
@@ -71,34 +87,55 @@ def download_file(url: str) -> str:
     return local_file
 
 
-def extract_image(version_and_path: (version.Version, str)) -> str:
+def extract_image(version_and_path: (version.Version, str), ide_name: str, jar_name: str, logo_path: str) -> str:
     name = str(version_and_path[0])
-    image_base_path = '../images/' + name
+    logo_path = logo_path.replace("<VERSION>", str(version_and_path[0]).replace(".", ""))
+    image_base_path = '../images/' + ide_name + "/" + name
+    result_path = image_base_path + "/" + logo_path
     if os.path.isdir(image_base_path):
         print("Skipping existing " + name)
-        return image_base_path
+        return result_path
     print("Working on " + name)
-    t = tarfile.open(version_and_path[1], "r:gz")
-    content_dir = os.path.commonprefix(t.getnames())
-    resources_name = content_dir + "lib/resources.jar"
-    resources_base_path = '../download/resources'
-    t.extract(resources_name, resources_base_path)
-    resources_zip = zipfile.ZipFile(resources_base_path + "/" + resources_name)
-    resources_zip.extract("idea_logo.png", image_base_path)
     try:
-        resources_zip.extract("idea_logo@2x.png", image_base_path)
+        t = tarfile.open(version_and_path[1], "r:gz")
+        content_dir = os.path.commonprefix(t.getnames())
+        resources_name = content_dir + "lib/" + jar_name + ".jar"
+        resources_base_path = '../download/resources'
+        t.extract(resources_name, resources_base_path)
+        resources_zip = zipfile.ZipFile(resources_base_path + "/" + resources_name)
+        resources_zip.extract(logo_path + ".png", image_base_path)
+        try:
+            resources_zip.extract(logo_path + "@2x.png", image_base_path)
+        except KeyError:
+            print("Skipping high res image for " + name + ", seems not to exist")
     except KeyError:
-        print("Skipping high res image for " + name + ", seems not to exist")
-    return image_base_path
+        print("Failed! There seems to be no image for that!")
+        pass
+    return result_path
 
 
-def generate_result(images: List[Tuple[version.Version, str]]):
+def generate_result(image_data: Dict[str, List[Tuple[version.Version, str]]]):
+    version_data: Dict[version.Version, Dict[str, str]] = {}  # verison -> (ide->img)
+    for ide_name in IDE_NAMES:
+        for img_data in image_data.get(ide_name, []):
+            v = img_data[0]
+            path = img_data[1]
+            if v not in version_data:
+                version_data[v] = {}
+            version_data[v][ide_name] = path
     with open("../index.html", "w") as f:
-        f.write("<html><head><title>IDEA Splash screens</title></head><body>")
-        for image in images:
-            name = str(image[0])
-            f.write("<h3>" + name + "</h3>")
-            f.write("<a href=images/" + name + "/idea_logo@2x.png><img src=images/" + name + "/idea_logo.png/></a>")
+        f.write("<html><head><title>JetBrains IDE Splash screens</title></head><body>")
+        f.write("<table>")
+        for version, ide_paths in reversed(sorted(version_data.items())):
+            f.write("<tr>")
+            f.write("<td>" + str(version) + "</td>")
+            for ide_name in IDE_NAMES:
+                path = ide_paths.get(ide_name, 'not_existing')
+                f.write("<td>")
+                f.write("<a href=images/" + path + "@2x.png><img src=images/" + path + ".png/></a>")
+                f.write("</td>")
+            f.write("</tr>")
+        f.write("</table>")
         f.write("</body></html>")
 
 
@@ -109,15 +146,22 @@ def print_marker(text: str):
 if __name__ == "__main__":
     print_marker("fetching latest release information")
     index_json = requests.get(INDEX).json()
-    downloads = get_major_download_links(index_json[0]['releases'])
 
-    print_marker("downloading releases")
-    local_releases = [(download[0], download_file(download[1])) for download in downloads.values()]
+    image_data = {}
+    for ide_info, ide_json in zip(IDE, index_json):
+        ide_name = ide_info[0]
+        print_marker("parsing info for " + ide_name)
+        downloads = get_major_download_links(ide_json['releases'])
 
-    print_marker("extracting images")
-    images = [(release[0], extract_image(release)) for release in local_releases]
+        print_marker("downloading releases for " + ide_name)
+        local_releases = [(version, download_file(download[1])) for (version, download) in downloads.items()]
+
+        print_marker("extracting images for " + ide_name)
+        images = [(release[0], extract_image(release, ide_name, ide_info[1], ide_info[2])) for release in local_releases]
+
+        image_data[ide_name] = images
 
     print_marker("Generating result")
-    generate_result(images)
+    generate_result(image_data)
 
     print_marker("All Done!")
